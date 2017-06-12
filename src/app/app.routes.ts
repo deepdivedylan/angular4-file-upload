660f6ea288826a713a26bb4f47219431af494eab
Dylan McDonald <dmcdonald21@cnm.edu>
2017-06-12 13:56:01 -0600

    added blank image component schtuff

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import {RouterModule, Routes} from "@angular/router";
 import {BaconComponent} from "./components/bacon-component";
 import {HomeComponent} from "./components/home-component";
 import {BaconService} from "./services/bacon-service";
+import {ImageComponent} from "./components/image-component";
 
-export const allAppComponents = [BaconComponent, HomeComponent];
+export const allAppComponents = [BaconComponent, HomeComponent, ImageComponent];
 
 export const routes: Routes = [
 	{path: "bacon", component: BaconComponent},
+	{path: "image", component: ImageComponent},
 	{path: "", component: HomeComponent}
 ];
 
