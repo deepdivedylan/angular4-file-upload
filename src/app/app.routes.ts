5e211b5dc267367b3a2439815f34c9e81afee190
Dylan McDonald <dmcdonald21@cnm.edu>
2017-06-12 14:31:02 -0600

    added FileSelectDirective globally

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,9 @@ import {BaconComponent} from "./components/bacon-component";
 import {HomeComponent} from "./components/home-component";
 import {BaconService} from "./services/bacon-service";
 import {ImageComponent} from "./components/image-component";
+import {FileSelectDirective} from "ng2-file-upload";
 
-export const allAppComponents = [BaconComponent, HomeComponent, ImageComponent];
+export const allAppComponents = [BaconComponent, HomeComponent, ImageComponent, FileSelectDirective];
 
 export const routes: Routes = [
 	{path: "bacon", component: BaconComponent},
